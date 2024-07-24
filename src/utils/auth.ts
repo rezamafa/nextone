ca0715375155b3f3c6da3c755dcf9df28964afa6
rezamafa <mafakherian.r@gmail.com>
2024-07-24 13:23:37 +0330

    Add google provider to next-auth.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,17 @@
 
-import { AuthOptions, getServerSession } from "next-auth"
-import Github from "next-auth/providers/github"
+import { AuthOptions, getServerSession } from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 const authOptions: AuthOptions = {
   providers: [
-        Github({
+    Github({
       clientId: process.env.GITHUB_ID  ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
     }),
   ],
 }
